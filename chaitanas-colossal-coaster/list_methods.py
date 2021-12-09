edd233db47b282444d72cf74e59a7a6622db3e89
Maider <maider.abad97@gmail.com>
2021-12-09 18:46:11 +0100

    Remove spaces

--- chaitanas-colossal-coaster/list_methods.py
+++ chaitanas-colossal-coaster/list_methods.py
@@ -1,6 +1,5 @@
 def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     """
-
     :param express_queue: list - names in the Fast-track queue.
     :param normal_queue:  list - names in the normal queue.
     :param ticket_type:  int - type of ticket. 1 = express, 0 = normal.
@@ -16,7 +15,6 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
 
 def find_my_friend(queue, friend_name):
     """
-
     :param queue: list - names in the queue.
     :param friend_name: str - name of friend to find.
     :return: int - index at which the friends name was found.
@@ -26,7 +24,6 @@ def find_my_friend(queue, friend_name):
 
 def add_me_with_my_friends(queue, index, person_name):
     """
-
     :param queue: list - names in the queue.
     :param index: int - the index at which to add the new name.
     :param person_name: str - the name to add.
@@ -38,7 +35,6 @@ def add_me_with_my_friends(queue, index, person_name):
 
 def remove_the_mean_person(queue, person_name):
     """
-
     :param queue: list - names in the queue.
     :param person_name: str - name of mean person.
     :return:  list - queue update with the mean persons name removed.
@@ -48,7 +44,6 @@ def remove_the_mean_person(queue, person_name):
 
 def how_many_namefellows(queue, person_name):
     """
-
     :param queue: list - names in the queue.
     :param person_name: str - name you wish to count or track.
     :return:  int - the number of times the name appears in the queue.
@@ -58,7 +53,6 @@ def how_many_namefellows(queue, person_name):
 
 def remove_the_last_person(queue):
     """
-
     :param queue: list - names in the queue.
     :return: str - name that has been removed from the end of the queue.
     """
@@ -67,7 +61,6 @@ def remove_the_last_person(queue):
 
 def sorted_names(queue):
     """
-
     :param queue: list - names in the queue.
     :return: list - copy of the queue in alphabetical order.
     """
